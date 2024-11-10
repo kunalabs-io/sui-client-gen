7ed48d9ab26867d311917ebdb643d0adc16b31c4
shiqi cao <shiqicao@gmail.com>
2024-11-10 13:14:54 -0800

    add type

--- generator/src/framework_sources.rs
+++ generator/src/framework_sources.rs
@@ -135,7 +135,7 @@ export function splitGenericParameters(
   genericSeparators: [string, string] = ['<', '>']
 ) {
   const [left, right] = genericSeparators
-  const tok = []
+  const tok: string[] = []
   let word = ''
   let nestedAngleBrackets = 0
 
